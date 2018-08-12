3707e0643351208eef19e94c29bf8019377fd12d
trisjones <trisjones04@gmail.com>
2018-08-12 15:08:39 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+  validate :valid_clickbait
+
+  def valid_clickbait
+    return false unless self.title
+    array = ["Won't Believe", "Secret", /Top \d/, "Guess"]
+    array.each{|str|
+      return true if title.match(str)
+    }
+    errors.add(:title, "non clickbait")
+  end
+  
+  # validates_each :title do |record, attr, value|
+  #   record.errors.add(attr, "must be clickbait") if
+  #   ["Won't Believe", "Secret", /Top \d/, "Guess"].any?{|bait| :title.include?(bait)}
+  # end
+
 end
